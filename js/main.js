b0a9b99745873904984488d1cceb903e4d8f5eb5
MurtazaN <mnipplewala@worcester.edu>
2022-06-13 20:48:26 -0400

    product demo v3

--- js/main.js
+++ js/main.js
@@ -6,13 +6,13 @@ async function makeReq(){
   const res = await fetch(`/api/${_id}`)
   const data = await res.json()
 
-  console.log(data)
-  document.querySelector('#bookIsbn').textContent = data.isbn
-//   document.querySelector("#bookTitle").textContent = data.title
-//   document.querySelector("#bookIsbn13").textContent = data.isbn13
-//   document.querySelector("#bookIsbn").textContent = data.isbn
-//   document.querySelector("#bookImage").textContent = data.image
-//   document.querySelector("#book_id").textContent = data._id
-//   document.querySelector("#bookLink").textContent = data.link
-//   document.querySelector("#bookCustomer_rating").textContent = data.customer_rating
+  console.log(data);
+  document.querySelector('#bookTitle').textContent = "Title: " + data.title
+  document.querySelector('#bookIsbn13').textContent = "ISBN13: " + data.isbn13
+  document.querySelector('#bookIsbn').textContent = "ISBN: " + data.isbn
+  document.querySelector('#bookImage').src = data.image
+  document.querySelector('#bookId').textContent = "Book ID: " + data.id
+  document.querySelector('#bookLink').href = data.link
+  document.querySelector('#bookLink').innerText = "Book link: "
+  document.querySelector('#bookCustomer_rating').textContent = "Customer Rating: " + data.customer_rating
 }
